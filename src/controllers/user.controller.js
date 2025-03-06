fd296cb1c978c13fa3156d7dc0f19c79038e4fb5
prakhar <prakharagarwal1434@gmail.com>
2025-03-06 23:48:49 +0530

    getting the data

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler( async(req,res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    //get user data from frontend
+    //validaton checks
+    //check if user already exists
+    //check for image and avatar
+    //upload image to cloudinary,avatar
+    //create user object - create entry in db
+    //remove password and refresh token field from res
+    //return res
+    const {fullname,email,username,password}=req.body
+    console.log("email:",email);
 })
 
 export {registerUser}
