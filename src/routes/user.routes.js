9cbcfc68efe2a994e9a02d1665ed494c0caee4f7
prakhar <prakharagarwal1434@gmail.com>
2025-03-07 17:10:30 +0530

    logout functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.midlewares.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 router.route("/register").post(
@@ -16,4 +17,9 @@ router.route("/register").post(
     ]),
     registerUser
     )
-export default router
+
+router.route("/login").post(loginUser)
+
+//secured routes 
+router.route("/logout").post(verifyJWT, logoutUser)
+export   default router
